0ed58b493dc150c9d4eddc9794522d7a0d5be4c3
“sunmingxin” <sunmingxin@zuoyebang.com>
2020-11-08 22:12:02 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       repo : 'https://github.com/mission-sun/mission-blog.git',
       path : '/home/root-mission/blog/web',
       'pre-deploy': "git fetch",
-      'post-deploy' : 'npm install --registry=https://registry.npm.taobao.org && npm run build && mv -f dist ./..'
+      'post-deploy' : 'npm install --registry=https://registry.npm.taobao.org && npm run build && rm -rf ./../dist && mv -f dist ./..'
     }
   }
 };
